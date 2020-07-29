478206a06ae181ec0525bf1645e1bb4284784698
laczkobalazs <laczko.balazs89@gmail.com>
2020-07-29 16:54:20 +0200

    adds removeFromWatchedMovies feature and when we add a movie to Watched which is already in the Watchlist, it automatically deletes from prev list

--- moviedb/src/components/Movie.js
+++ moviedb/src/components/Movie.js
@@ -31,9 +31,53 @@ function Movie(props) {
     }
   };
 
+  const [deleteUndoButton2, setDeleteUndoButton2] = useState(
+    "Remove from Watched List"
+  );
+
+  const removeFromWatched = (e) => {
+    axios.get(`http://localhost:8080/delete/watched-movie/${props.movie.id}`);
+  };
+
   const addToWatchedMovies = () => {
     const url = `http://localhost:8080/add/watched-movie/${props.movie.id}`;
     axios.get(url);
+  };
+
+  const decideEventWatched = () => {
+    switch (deleteUndoButton2) {
+      case "Remove from Watched List":
+        removeFromWatched();
+        setDeleteUndoButton2("Undo");
+        break;
+      default:
+        addToWatchedMovies();
+        setDeleteUndoButton2("Remove from watch list");
+        break;
+    }
+  };
+
+  const [deleteUndoButton3, setDeleteUndoButton3] = useState(
+    "Add to Watched List"
+  );
+
+  const decideEventAddToWatched = () => {
+    switch (deleteUndoButton3) {
+      case "Add to Watched List":
+        addToWatchedMovies();
+        setDeleteUndoButton3("Undo");
+        break;
+      default:
+        removeFromWatchedAddToWatch();
+        setDeleteUndoButton3("Add to Watched List");
+        break;
+    }
+  };
+
+  const removeFromWatchedAddToWatch = (e) => {
+    axios
+      .get(`http://localhost:8080/delete-watched/add-watch/${props.movie.id}`)
+      .then((res) => console.log(res));
   };
 
   return (
@@ -48,7 +92,16 @@ function Movie(props) {
           {deleteUndoButton}
         </button>
       )}
-      <button onClick={addToWatchedMovies}>Add to Watched Movies.</button>
+
+      {window.location.pathname !== "/watched-list" ? (
+        <button type="button" onClick={decideEventAddToWatched}>
+          {deleteUndoButton3}
+        </button>
+      ) : (
+        <button type="button" onClick={decideEventWatched}>
+          {deleteUndoButton2}
+        </button>
+      )}
     </div>
   );
 }
