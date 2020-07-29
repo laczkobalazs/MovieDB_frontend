f2bf5a0148c39b1d5efc9e482e7f23c599043206
laczkobalazs <laczko.balazs89@gmail.com>
2020-07-29 14:28:37 +0200

    adds addToWatchedMovies button

--- moviedb/src/components/Movie.js
+++ moviedb/src/components/Movie.js
@@ -1,7 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 function Movie(props) {
   console.log("movie.js vagyok: " + props.movie);
+
+  const addToWatchedMovies = () => {
+    const url = `http://localhost:8080/add/watched-movie/${props.movie.id}`;
+    axios.get(url);
+  };
+
   return (
     <div>
       <h1>{props.movie.title}</h1>
@@ -9,6 +16,8 @@ function Movie(props) {
       <p>{props.movie.release_date}</p>
       <p>{props.movie.popularity}</p>
       <p>{props.movie.overview}</p>
+      <p>{props.movie.id}</p>
+      <button onClick={addToWatchedMovies}>Add to Watched Movies.</button>
     </div>
   );
 }
