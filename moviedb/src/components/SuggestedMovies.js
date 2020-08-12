77af9eca7ab38b4728ce3b719eec1cd3e2a25b05
laczkobalazs <laczko.balazs89@gmail.com>
2020-08-12 16:52:53 +0200

    adds toggle switch for suggested and not-suggested

--- moviedb/src/components/SuggestedMovies.js
+++ moviedb/src/components/SuggestedMovies.js
@@ -6,15 +6,42 @@ import { LanguageContext } from "../context/LanguageContext";
 export default function SuggestedMovies() {
   const [suggestedMovies, setSuggestedMovies] = useState([]);
   const [language, setLanguage] = useContext(LanguageContext);
+  const [movieType, setMovieType] = useState("suggested");
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8080/recommendation/suggested/${language}`)
-      .then((res) => setSuggestedMovies(res.data.results));
-  }, [language]);
+    if (movieType === "suggested") {
+      axios
+        .get(`http://localhost:8080/recommendation/suggested/${language}`)
+        .then((res) => setSuggestedMovies(res.data.results));
+    } else {
+      axios
+        .get(`http://localhost:8080/recommendation/not-suggested/${language}`)
+        .then((res) => setSuggestedMovies(res.data.results));
+    }
+  }, [language, movieType]);
 
   return (
     <div>
+      <button
+        style={{ borderRadius: "0.3rem", backgroundColor: "green" }}
+        disabled={movieType === "suggested"}
+        onClick={() => {
+          setMovieType("suggested");
+        }}
+      >
+        Watch!
+      </button>
+
+      <button
+        style={{ borderRadius: "0.3rem", backgroundColor: "red" }}
+        disabled={movieType === "not suggested"}
+        onClick={() => {
+          setMovieType("not suggested");
+        }}
+      >
+        Don't watch!
+      </button>
+
       <MovieList movies={suggestedMovies} />
     </div>
   );
