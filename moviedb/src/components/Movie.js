f931ed90e666f2384799cf2ea421eff3f894b326
Viktor Bocz <beviktor95@gmail.com>
2020-08-13 10:33:20 +0200

    Fixed movie card

--- moviedb/src/components/Movie.js
+++ moviedb/src/components/Movie.js
@@ -148,8 +148,8 @@ function Movie({ movie }) {
   return (
     <div className="movie-container">
       <div className="cellphone-container">
-        <Link to={`/movie/${movie.id}`}>
-          <div className="movie">
+        <div className="movie">
+          <Link to={`/movie/${movie.id}`}>
             <div className="movie-img">
               {movie.poster_path ? (
                 <img
@@ -167,90 +167,89 @@ function Movie({ movie }) {
                 />
               )}
             </div>
-            <div className="text-movie-cont">
-              <div className="mr-grid">
-                <div className="col1">
+          </Link>
+          <div className="text-movie-cont">
+            <div className="mr-grid">
+              <div className="col1">
+                <Link to={`/movie/${movie.id}`}>
                   {movie.title ? (
                     <h2>{movie.title}</h2>
                   ) : (
                     <h2>{movie.original_title}</h2>
                   )}
-                  {window.location.pathname === "/watched-list" ? (
-                    <Rating
-                      id={movie.id}
-                      prevRating={movie.vote_average}
-                      voteCount={movie.vote_count}
-                    />
-                  ) : (
-                    <div></div>
-                  )}
-                  <ul className="movie-gen">
-                    <li>{movie.release_date} /</li>
-                    <li>popularity: {movie.popularity} /</li>
-                    <li>language: {movie.original_language}</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mr-grid summary-row">
-                <div className="col2">
-                  <h5>SUMMARY</h5>
-                </div>
-                <img
-                  className="likeDislike"
-                  onClick={addToLikedMovieList}
-                  src={
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"
-                  }
-                  alt=""
-                />
-                <img
-                  className="likeDislike"
-                  onClick={addToDislikedMovieList}
-                  src={
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Broken_heart.svg/586px-Broken_heart.svg.png"
-                  }
-                  alt=""
-                />
-              </div>
-              <div className="mr-grid">
-                <div className="col1">
-                  <p className="movie-description">{shortenedOverview}</p>
-                </div>
-              </div>
-              <div className="mr-grid action-row">
-                <div className="col2">
-                  {isItWatchList() ? (
-                    <div
-                      className="watch-btn"
-                      onClick={decideEventAddToWatched}
-                    >
-                      <h3>{deleteUndoButton3}</h3>
-                    </div>
-                  ) : (
-                    <div className="watch-btn" onClick={decideEventWatched}>
-                      <h3>{deleteUndoButton2}</h3>
-                    </div>
-                  )}
-                  {isItWatchedList() ? (
-                    <div
-                      className="watch-btn"
-                      onClick={decideEventAddToWatchList}
-                    >
-                      <h3>{deleteUndoButton4}</h3>
-                    </div>
-                  ) : (
-                    <div className="watch-btn" onClick={decideEvent}>
-                      <h3>{deleteUndoButton}</h3>
-                    </div>
-                  )}
-                </div>
-                <div className="col6 action-btn"></div>
-                <div className="col6 action-btn"></div>
-                <div className="col6 action-btn"></div>
+                </Link>
+                {window.location.pathname === "/watched-list" ? (
+                  <Rating
+                    id={movie.id}
+                    prevRating={movie.vote_average}
+                    voteCount={movie.vote_count}
+                  />
+                ) : (
+                  <div></div>
+                )}
+                <ul className="movie-gen">
+                  <li>{movie.release_date} /</li>
+                  <li>popularity: {movie.popularity} /</li>
+                  <li>language: {movie.original_language}</li>
+                </ul>
               </div>
             </div>
+            <div className="mr-grid summary-row">
+              <div className="col2">
+                <h5>SUMMARY</h5>
+              </div>
+              <img
+                className="likeDislike"
+                onClick={addToLikedMovieList}
+                src={
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"
+                }
+                alt=""
+              />
+              <img
+                className="likeDislike"
+                onClick={addToDislikedMovieList}
+                src={
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Broken_heart.svg/586px-Broken_heart.svg.png"
+                }
+                alt=""
+              />
+            </div>
+            <div className="mr-grid">
+              <div className="col1">
+                <p className="movie-description">{shortenedOverview}</p>
+              </div>
+            </div>
+            <div className="mr-grid action-row">
+              <div className="col2">
+                {isItWatchList() ? (
+                  <div className="watch-btn" onClick={decideEventAddToWatched}>
+                    <h3>{deleteUndoButton3}</h3>
+                  </div>
+                ) : (
+                  <div className="watch-btn" onClick={decideEventWatched}>
+                    <h3>{deleteUndoButton2}</h3>
+                  </div>
+                )}
+                {isItWatchedList() ? (
+                  <div
+                    className="watch-btn"
+                    onClick={decideEventAddToWatchList}
+                  >
+                    <h3>{deleteUndoButton4}</h3>
+                  </div>
+                ) : (
+                  <div className="watch-btn" onClick={decideEvent}>
+                    <h3>{deleteUndoButton}</h3>
+                  </div>
+                )}
+              </div>
+              <div className="col6 action-btn"></div>
+              <div className="col6 action-btn"></div>
+              <div className="col6 action-btn"></div>
+            </div>
           </div>
-        </Link>
+        </div>
       </div>
     </div>
   );
