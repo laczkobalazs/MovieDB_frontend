f952108d85bb3d4f2b8af39b47633007ab1454fe
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-08-13 09:19:36 +0200

    movie cards organized

--- moviedb/src/components/MovieList.js
+++ moviedb/src/components/MovieList.js
@@ -3,7 +3,14 @@ import Movie from "./Movie";
 
 export default function MovieList(props) {
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       {props.movies.map((movie) => (
         <Movie movie={movie} key={movie.id} />
       ))}
