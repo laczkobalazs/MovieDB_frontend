ce42b62eb66638a07fa3b1a1a0651dd090e82a44
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-08-24 16:19:51 +0200

    login not working because of cors policy

--- moviedb/src/components/Login.js
+++ moviedb/src/components/Login.js
@@ -5,20 +5,22 @@ function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const headers = {
-    "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+  const config = {
+    headers: {
+      Origin: "http://localhost:3000/",
+      "Access-Control-Request-Method": "POST, OPTIONS",
+      "Access-Control-Request-Headers": "access-control-allow-origin",
+    },
   };
-
+  //'X-Requested-With': 'http://localhost:3000/',
+  //"http://localhost:8080/auth/sign-in"
   const sendUserLoginData = () => {
     console.log(username + " " + password);
-    Axios.post("http://localhost:8080/auth/sign-in", {
-      mode: "no-cors",
-      headers: headers,
-      username: username,
-      password: password,
-      // withCredentials: true,
-      // credentials: "same-origin",
+    Axios({
+      method: "post",
+      url: "http://localhost:8080/auth/sign-in",
+      data: { username: username, password: password },
+      headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" },
     }).then((data) => {
       console.log(data.data);
       document.cookie(`Authorization=Bearer ${data.data.token}`);
