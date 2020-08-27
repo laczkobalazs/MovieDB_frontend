3f0abb1a64c2060cc70e7ec84c44ff75f82b8bc5
Viktor Bocz <beviktor95@gmail.com>
2020-08-27 11:00:46 +0200

    Delete users by admin

--- moviedb/src/components/Users.js
+++ moviedb/src/components/Users.js
@@ -1,27 +1,78 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import "../style/Users.scss";
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const cookieValue = document.cookie.split("=")[1];
+
+  const receiveUsers = (url) => {
+    Axios.get(url, {
+      withCredentials: true,
+      headers: { Authorization: cookieValue },
+    }).then((data) => {
+      setUsers(data.data);
+    });
+  };
 
   useEffect(() => {
     const url = "http://localhost:8080/users";
-    Axios.get(url).then((data) => {
-      console.log(data.data);
-      setUsers(data.data);
-    });
-  }, []);
+    const redirectUrl = "http://localhost:3000";
+    if (!cookieValue) {
+      window.location.replace(redirectUrl);
+    }
+    Axios.get(url, {
+      withCredentials: true,
+      headers: { Authorization: cookieValue },
+    })
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((e) => {
+        window.location.replace(redirectUrl);
+      });
+  }, [cookieValue]);
+
+  const deleteUser = (id) => {
+    const url = `http://localhost:8080/users/${id}`;
+    Axios.delete(url).then(receiveUsers("http://localhost:8080/users"));
+  };
 
   return (
     <div>
-      {users.map((user) => (
-        <p>
-          Username: {user.userName} | Email address: {user.email} | Roles:
-          {user.roles}
-        </p>
-      ))}
+      {users.length !== 0 ? (
+        <table className="rwd-table">
+          <thead>
+            <tr>
+              <td className="head-column">ID</td>
+              <td className="head-column">Username</td>
+              <td className="head-column">Email address</td>
+              <td className="head-column">Roles</td>
+              <td className="head-column">Delete</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.userName}</td>
+                <td>{user.email}</td>
+                <td>{user.roles}</td>
+                {user.roles === "[ROLE_USER]" ? (
+                  <td>
+                    <button onClick={() => deleteUser(user.id)}>X</button>
+                  </td>
+                ) : (
+                  <td></td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <span></span>
+      )}
     </div>
   );
 }
-
 export default Users;
